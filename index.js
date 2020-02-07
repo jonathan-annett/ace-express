e4cbb3e9539689cff9014cc25c12b45ef35a41b8
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-07 15:02:38 +1100

    auto updated

--- index.js
+++ index.js
@@ -337,36 +337,355 @@ function debugButtonTextFromFilename(filename) {
 function getEditorMasterHTML (files,title,theme) {
 
     function masterHTMLBrowserCode() {
+        
+        var file_index;
+        
+        function setupSplitter() {
+            
+            (function () {
+            
+                /**
+                 * THIS OBJECT WILL ONLY WORK IF your target is positioned relative or absolute,
+                 * or anything that works with the top and left css properties (not static).
+                 *
+                 * Howto
+                 * ============
+                 *
+                 * document.getElementById('my_target').sdrag();
+                 *
+                 * onDrag, onStop
+                 * -------------------
+                 * document.getElementById('my_target').sdrag(onDrag, null);
+                 * document.getElementById('my_target').sdrag(null, onStop);
+                 * document.getElementById('my_target').sdrag(onDrag, onStop);
+                 *
+                 * Both onDrag and onStop callback take the following arguments:
+                 *
+                 * - el, the currentTarget element (#my_target in the above examples)
+                 * - pageX: the mouse event's pageX property (horizontal position of the mouse compared to the viewport)
+                 * - startX: the distance from the element's left property to the horizontal mouse position in the viewport.
+                 *                  Usually, you don't need to use that property; it is internally used to fix the undesirable
+                 *                  offset that naturally occurs when you don't drag the element by its top left corner
+                 *                  (for instance if you drag the element from its center).
+                 * - pageY: the mouse event's pageX property (horizontal position of the mouse compared to the viewport)
+                 * - startY: same as startX, but for the vertical axis (and element's top property)
+                 *
+                 *
+                 *
+                 * The onDrag callback accepts an extra argument: fix.
+                 *
+                 * fix is an array used to fix the coordinates applied to the target.
+                 *
+                 * It can be used to constrain the movement of the target inside of a virtual rectangle area for instance.
+                 * Put a variable in the fix array to override it.
+                 * The possible keys are:
+                 *
+                 * - pageX
+                 * - startX
+                 * - pageY
+                 * - startY
+                 * - skipX
+                 * - skipY
+                 *
+                 * skipX and skipY let you skip the updating of the target's left property.
+                 * This might be required in some cases where the positioning of the target
+                 * is automatically done by the means of other css properties.
+                 *
+                 * 
+                 *
+                 *
+                 *
+                 *
+                 * Direction
+                 * -------------
+                 * With direction, you can constrain the drag to one direction only: horizontal or vertical.
+                 * Accepted values are:
+                 *
+                 * - <undefined> (the default)
+                 * - vertical
+                 * - horizontal
+                 *
+                 *
+                 *
+                 *
+                 */
+            
+                // simple drag
+                function sdrag(onDrag, onStop, direction) {
+            
+                    var startX = 0;
+                    var startY = 0;
+                    var el = this;
+                    var dragging = false;
+            
+                    function move(e) {
+            
+                        var fix = {};
+                        onDrag && onDrag(el, e.pageX, startX, e.pageY, startY, fix);
+                        if ('vertical' !== direction) {
+                            var pageX = ('pageX' in fix) ? fix.pageX : e.pageX;
+                            if ('startX' in fix) {
+                                startX = fix.startX;
+                            }
+                            if (false === ('skipX' in fix)) {
+                                el.style.left = (pageX - startX) + 'px';
+                            }
+                        }
+                        if ('horizontal' !== direction) {
+                            var pageY = ('pageY' in fix) ? fix.pageY : e.pageY;
+                            if ('startY' in fix) {
+                                startY = fix.startY;
+                            }
+                            if (false === ('skipY' in fix)) {
+                                el.style.top = (pageY - startY) + 'px';
+                            }
+                        }
+                    }
+            
+                    function startDragging(e) {
+                        if (e.currentTarget instanceof HTMLElement || e.currentTarget instanceof SVGElement) {
+                            dragging = true;
+                            var left = el.style.left ? parseInt(el.style.left) : 0;
+                            var top = el.style.top ? parseInt(el.style.top) : 0;
+                            startX = e.pageX - left;
+                            startY = e.pageY - top;
+                            window.addEventListener('mousemove', move);
+                        }
+                        else {
+                            throw new Error("Your target must be an html element");
+                        }
+                    }
+            
+                    this.addEventListener('mousedown', startDragging);
+                    window.addEventListener('mouseup', function (e) {
+                        if (true === dragging) {
+                            dragging = false;
+                            window.removeEventListener('mousemove', move);
+                            onStop && onStop(el, e.pageX, startX, e.pageY, startY);
+                        }
+                    });
+                }
+            
+                Element.prototype.sdrag = sdrag;
+            })();
+           
+           var leftPane = document.getElementById('left-pane');
+           var rightPane = document.getElementById('editor');
+           var paneSep = document.getElementById('panes-separator');
+       
+           // The script below constrains the target to move horizontally between a left and a right virtual boundaries.
+           // - the left limit is positioned at 10% of the screen width
+           // - the right limit is positioned at 90% of the screen width
+           var leftLimit = 10;
+           var rightLimit = 90;
+       
+       
+           paneSep.sdrag(function (el, pageX, startX, pageY, startY, fix) {
+       
+               fix.skipX = true;
+       
+               if (pageX < window.innerWidth * leftLimit / 100) {
+                   pageX = window.innerWidth * leftLimit / 100;
+                   fix.pageX = pageX;
+               }
+               if (pageX > window.innerWidth * rightLimit / 100) {
+                   pageX = window.innerWidth * rightLimit / 100;
+                   fix.pageX = pageX;
+               }
+       
+               var cur = pageX / window.innerWidth * 100;
+               if (cur < 0) {
+                   cur = 0;
+               }
+               if (cur > window.innerWidth) {
+                   cur = window.innerWidth;
+               }
+       
+       
+               var right = (100-cur-2);
+               leftPane.style.width = cur + '%';
+               rightPane.style.width = right + '%';
+               
+               if (rightPane.children[0] && !rightPane.children[0].hidden) {
+                    rightPane.children[0].hidden=true;   
+               }
+               
+       
+           }, 
+           
+           function () {
+             if(rightPane.children[0]) rightPane.children[0].hidden=false;
+           }
+           , 'horizontal');
+       
+       
+           
+           
+        }
+        
+        
+        
+        function resolveFile(file) {
+            if (typeof file==='object' && file.target) {
+                if (file.preventDefault) file.preventDefault();
+                if (file.stopImmediatePropagation) file.stopImmediatePropagation();
+                return file.target.dataset.fileEdit;
+            }
+            return file;
+        }
 
 
         function editFile (file) {
-            if (typeof file==='object' && file.target) {
-                if (file.preventDefault) file.preventDefault();
-                if (file.stopImmediatePropagation) file.stopImmediatePropagation();
-                file = file.target.dataset.fileEdit;
-            }
-            window.open(ace_single_file_edit_url+file,"_blank", "scrollbars=1,fullscreen=yes,status=no,toolbar=no,menubar=no,location=no");
+            document.getElementById("editor").innerHTML='<object type="text/html" data="'+ace_single_file_edit_url+resolveFile(file)+'"></object>'
+        }
+
+        function openFile (file) {
+            window.open(ace_single_file_edit_url+resolveFile(file),"_blank", "scrollbars=1,fullscreen=yes,status=no,toolbar=no,menubar=no,location=no");
         }
 
         function debugFile (file) {
-            if (typeof file==='object' && file.target) {
-                if (file.preventDefault) file.preventDefault();
-                if (file.stopImmediatePropagation) file.stopImmediatePropagation();
-                file = file.target.dataset.fileDebug;
-            }
-            window.open(ace_single_file_debug_url+file,"_blank", "scrollbars=1,fullscreen=yes,status=no,toolbar=no,menubar=no,location=no");
+            window.open(ace_single_file_debug_url+resolveFile(file),"_blank", "scrollbars=1,fullscreen=yes,status=no,toolbar=no,menubar=no,location=no");
         }
-
 
         function serveFile (file) {
-            if (typeof file==='object' && file.target) {
-                if (file.preventDefault) file.preventDefault();
-                if (file.stopImmediatePropagation) file.stopImmediatePropagation();
-                file = file.target.dataset.fileServe;
-            }
-            window.open(ace_single_file_serve_url+file,"_blank", "scrollbars=1,fullscreen=yes,status=no,toolbar=no,menubar=no,location=no");
+            window.open(ace_single_file_serve_url+resolveFile(file),"_blank", "scrollbars=1,fullscreen=yes,status=no,toolbar=no,menubar=no,location=no");
         }
-
+        
+        function sub(files, dir) {
+          var dir_len = dir.length;
+          return files
+            .filter(function(fn) {
+              return fn.startsWith(dir);
+            })
+            .map(function(fn) {
+              return fn.substr(dir_len);
+            });
+        }
+        
+        function topFiles(files) {
+          return files
+            .map(function(fn) {
+              var i = fn.indexOf("/");
+              return i < 0 ? fn : fn.substr(0, i + 1);
+            })
+            .filter(function(fn, ix, ar) {
+              return ar.indexOf(fn) === ix;
+            })
+            .map(function(fn) {
+              if (fn.endsWith("/")) {
+                var subfiles = sub(files, fn);
+                return { dir: fn.substr(0, fn.length - 1), files: topFiles(subfiles) };
+              } else {
+                return fn;
+              }
+            });
+        }
+        
+        function dirHtml(files, ids, cb, root, ind) {
+          root = root || "";
+          ind = ind || "";
+          var h = "";
+          files.forEach(function(f) {
+            var i = "f_" + (Object.keys(ids).length + 1).toString(36);
+            if (typeof f === "string") {
+              ids[i] = root + f;
+              h += ind + '<input type="radio" name="hosted_files" id="'+i+'" value="'+f+'">\n';
+              h += ind + '<label onclick="' + cb + '(this.htmlFor);" for="'+i+'">'+f+"</label>\n";
+            } else {
+              var dir = root + f.dir;
+              ids[i] = dir;
+        
+              h += ind + '<input type="checkbox" id="' + i + '">\n';
+              h += ind + '<label for="' + i + '">' + f.dir + "</label>\n";
+              h += ind + '<div class="dir_wrapper">\n';
+              h += dirHtml(f.files, ids, cb, root + f.dir + "/", ind + " ");
+              h += ind + "</div>\n";
+            }
+          });
+          return h;
+        }
+        
+        function loadfiles(el, files, cb) {
+          var ids = {};
+          var list = topFiles(files);
+          var html = dirHtml(list, ids, cb);
+        
+          el.innerHTML = html;
+          return ids;
+        }
+        
+        function fileTreeClick(id) {
+            editFile (file_index[id]);
+        }
+        
+        function setupMenu () {
+            
+            var 
+            
+            menu = document.querySelector('.menu'),
+            currentFile;
+            
+            
+            document.getElementById("menu_file_open").addEventListener("mousedown",function(){
+                if (currentFile) {
+                    editFile(currentFile);
+                }
+            },false);
+            
+            
+            document.getElementById("menu_file_open_new").addEventListener("mousedown",function(){
+                if (currentFile) {
+                    openFile(currentFile);
+                }
+            },false);
+            
+            document.getElementById("menu_file_debug").addEventListener("mousedown",function(){
+                if (currentFile) {
+                    debugFile(currentFile);
+                }
+            },false);
+            
+            document.getElementById("menu_file_serve").addEventListener("mousedown",function(){
+                if (currentFile) {
+                    serveFile(currentFile);
+                }
+            },false);
+            
+            function showMenu(x, y){
+                menu.style.left = x + 'px';
+                menu.style.top = y + 'px';
+                menu.classList.add('menu-show');
+            }
+            
+            function hideMenu(){
+                menu.classList.remove('menu-show');
+            }
+            
+            function onContextMenu(e){
+                console.log(e.target);
+                e.preventDefault();
+                if (e.target && e.target.htmlFor && file_index[e.target.htmlFor] && (currentFile = file_index[e.target.htmlFor]) ) {
+                    
+                    showMenu(e.pageX, e.pageY);
+                    document.addEventListener('mousedown', onMouseDown, false);
+                } else {
+                    currentFile=null;
+                }
+            }
+            
+            function onMouseDown(e){
+                hideMenu();
+                document.removeEventListener('mousedown', onMouseDown);
+            }
+            
+            document.addEventListener('contextmenu', onContextMenu, false);
+            
+        }
+        
+        file_index = loadfiles(document.querySelector(".file_tree"), Object.keys(files), "fileTreeClick");
+        
+        setupSplitter();
+        setupMenu ();
 
         document.querySelectorAll("[data-file-edit]").forEach(
             function(btn) {
@@ -423,8 +742,7 @@ function getEditorMasterHTML (files,title,theme) {
 
     }
 
-
-   function getFiles () {
+    function getFiles () {
         var fileIndex = {};
         files.forEach(function(file){
             var filename = typeof file ==='string' ? file : file.file;
@@ -482,7 +800,8 @@ function getEditorMasterHTML (files,title,theme) {
 
         html.append(
           ace_directory_html_template.renderWithObject(Object.values(filesNow)),
-          "table");
+          "table"
+        );
 
         html.append(masterHTMLBrowserCode,"body");
         res.send(html.html);
@@ -650,18 +969,7 @@ function fileEditor(theme,file,app,append_html) {
             res.redirect(ace_single_file_serve_url+file);
         });
 
-
-
-
     app.use(ace_single_file_serve_url,express.static(path.resolve(".")));
-
-    app.use(
-        "/jszip_test.zip",
-        express.static(path.resolve("./node_modules/jsextensions/src/jszip_test.zip"))
-    );
-
-
-
 
     app.post("/edited/"+file,editedCallback);
 
@@ -831,7 +1139,6 @@ function multiFileEditor(theme,files,port,append_html) {
     Function.startServer(app,function(){
         var listener = app.listen(port||0, function() {
 
-
             var connected = {},ids=[];
 
             files.forEach(function(file){
@@ -886,7 +1193,8 @@ function multiFileEditor(theme,files,port,append_html) {
                 return (when - Number.parseInt(digits,36))/1000;
             }
 
-            function wsIdAgeTest() {
+        /*    
+           function wsIdAgeTest() {
 
                 setTimeout(function(id){
                     var age = wsAge(id);
@@ -898,7 +1206,7 @@ function multiFileEditor(theme,files,port,append_html) {
 
             }
 
-            wsIdAgeTest();
+           wsIdAgeTest();*/
 
             app.use(function (req, res, next) {
                  req.ws_id =wsId(10,6);
@@ -960,13 +1268,11 @@ function multiFileEditor(theme,files,port,append_html) {
         });
     });
 
-    return Object.defineProperties( self,
-        {
-            files               : { value : editors, enumerable:true},
-            addEventListener    : { value : addEventListener , enumerable:true} ,
-            removeEventListener : { value : removeEventListener, enumerable:true},
-        }
-    );
+    return Object.defineProperties(self,{
+        files               : { value : editors, enumerable:true},
+        addEventListener    : { value : addEventListener , enumerable:true} ,
+        removeEventListener : { value : removeEventListener, enumerable:true},
+    });
 }
 
 function nodeCLI(argv) {
