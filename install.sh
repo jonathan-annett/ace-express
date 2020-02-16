5c250134e161a5ee372eaba613314c2bf35cf7da
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-16 14:26:01 +1100

    tidied up install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-which node && which npm || echo "please install node.js"
-which npm && npm install
-which npm && which ace-express && npm remove -g
-which npm && install -g
+which node >/dev/null && which npm  >/dev/null || echo "please install node.js - see https://github.com/tj/n for help with that"
+which npm  >/dev/null  && npm install
+which npm  >/dev/null  && which ace-express && npm remove -g
+which npm  >/dev/null  && install -g
