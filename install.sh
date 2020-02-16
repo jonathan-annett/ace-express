105a0710eb50956ce657b9e1f110a0e982ff4f4e
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-16 14:29:26 +1100

    fixed issue with install.sh (doh!)

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 which node >/dev/null && which npm  >/dev/null || echo "please install node.js - see https://github.com/tj/n for help with that"
 which npm  >/dev/null  && npm install
 which npm  >/dev/null  && which ace-express && npm remove -g
-which npm  >/dev/null  && install -g
+which npm  >/dev/null  && npm install -g
