539578e838b72b11f64380b3504f671992d5a16d
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-18 09:17:39 +1100

    updated install.sh to refresh from git repo first

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-which node >/dev/null && which npm  >/dev/null || echo "please install node.js - see https://github.com/tj/n for help with that"
+which node >/dev/null  && which npm  >/dev/null || echo "please install node.js - see https://github.com/tj/n for help with that"
+which node >/dev/null  && which npm  >/dev/null && git pull
 which npm  >/dev/null  && npm install
 which npm  >/dev/null  && which ace-express && npm remove -g
 which npm  >/dev/null  && npm install -g
